c5e5bf7df9a6152d051c55b01409718352fbfa54
Ilya Groshev <groshev.i.d@gmail.com>
2018-09-05 16:54:55 +0300

    fix for running non from pwd

--- run.sh
+++ run.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
+myloc=$(dirname $0)
+echo $myloc
+
 docker run --rm --name eosio -d -p 8888:8888 -p 9876:9876 \
--v `pwd`/eosio-wallet:/root/eosio-wallet \
--v `pwd`:/localEnv \
+-v $myloc/eosio-wallet:/root/eosio-wallet \
+-v $myloc:/localEnv \
 eosio/eos-dev \
 /localEnv/indocker/startNode.sh
 
