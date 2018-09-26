a939b72a21236a37a185983ac68aacc51c5e0a5d
Ilya Groshev <groshev.i.d@gmail.com>
2018-09-26 21:21:56 +0100

    added fresh docker container

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@ docker run --rm --name eosio -d -p 8888:8888 -p 9876:9876 \
 -v ~:/work \
 -v `pwd`/eosio-wallet:/root/eosio-wallet-init \
 -v `pwd`:/localEnv \
-eosio/eos-dev:v1.2.5 \
+eosio/eos-dev:v1.3.0 \
 /localEnv/indocker/startNode.sh
 
 docker attach eosio
